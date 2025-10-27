a1f56ff3c222c93569798d5877c8459ad50469b2
Watchara Buasrirattana <watchara.buasrirattana@gmail.com>
2025-10-27 11:50:41 +0700

    overview

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -24,17 +24,57 @@ export default function Home() {
                     <h1>Overview</h1>
                     <p>Activity Summary</p>
                     <div className={styles.overviewItem}>
-                        <div className={styles.item1}>
-                            <h2>Task Completion</h2>
+                        <div className={styles.item}>
+                            <h3>Task Completion</h3>
+                            <p>Completed / Total</p>
+                            <div className={styles.count}>2/10</div>
+                            <button>
+                                <Image
+                                    src="/button.png"
+                                    alt="Button icon"
+                                    width={50}
+                                    height={50}
+                                />
+                            </button>
                         </div>
-                        <div className={styles.item1}>
-                            <h2>Task Completion</h2>
+                        <div className={styles.item}>
+                            <h3>This Week’s Schedule</h3>
+                            <p>Next on Fri 10 Oct | 22:00</p>
+                            <div className={styles.count}>2/10</div>
+                            <button>
+                                <Image
+                                    src="/button.png"
+                                    alt="Button icon"
+                                    width={50}
+                                    height={50}
+                                />
+                            </button>
                         </div>
-                        <div className={styles.item1}>
-                            <h2>Task Completion</h2>
+                        <div className={styles.item}>
+                            <h3>Habit Tracker </h3>
+                            <p>Completed Today</p>
+                            <div className={styles.count}>2/10</div>
+                            <button>
+                                <Image
+                                    src="/button.png"
+                                    alt="Button icon"
+                                    width={50}
+                                    height={50}
+                                />
+                            </button>
                         </div>
-                        <div className={styles.item1}>
-                            <h2>Task Completion</h2>
+                        <div className={styles.item}>
+                            <h3>Month Budget</h3>
+                            <p>Remaining Balance</p>
+                            <div className={styles.count}>2/10</div>
+                            <button>
+                                <Image
+                                    src="/button.png"
+                                    alt="Button icon"
+                                    width={50}
+                                    height={50}
+                                />
+                            </button>
                         </div>
                     </div>
                 </div>
